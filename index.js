476e57d835cce4f9d26e07e015b40c8c67387fa1
Lauren Jackson <rmcc0724@gmail.com>
2019-05-29 10:16:47 -0700

    Port fixed

--- index.js
+++ index.js
@@ -90,5 +90,5 @@ const server = http.createServer((req, res) => {
         }
     });
 });
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running in port ${PORT}`));
